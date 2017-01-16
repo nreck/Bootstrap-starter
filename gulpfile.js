2ef70ad34b5f879aeade6d2c2c1e47dab52af132
nreck <nor@Nicolajs-MacBook-Pro.local>
2017-01-16 14:08:29 +0100

    Changed file structure
    Changed the file structure for a better overview

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,30 @@ var rename = require('gulp-rename');
 
 gulp.task('styles', function() {
     //Compiles to CSS
-    gulp.src('./assets/sass/*.scss')
+    gulp.src('./www/assets/sass/*.scss')
         .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('./assets/css'))
+        .pipe(gulp.dest('./www/build'))
     //Compiles to minified CSS
-    gulp.src('./assets/sass/*.scss')
+    gulp.src('./www/assets/sass/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             extname: '.min.css'
         }))
-        .pipe(gulp.dest('./assets/css'))
+        .pipe(gulp.dest('./www/build'))
 });
 
 gulp.task('compress-js', function () {
     //Compiles to minified JS
-    gulp.src('./assets/js/scripts.js')
+    gulp.src('./www/assets/js/scripts.js')
         .pipe(uglify())
         .pipe(rename({
             extname: '.min.js'
         }))
-        .pipe(gulp.dest('./assets/js/'))
+        .pipe(gulp.dest('./www/build/'))
 })
 
 gulp.task('default',function() {
     //Watch over files
-    gulp.watch('./assets/sass/*.scss',['styles']);
-    gulp.watch('./assets/js/*.js',['compress-js']);
+    gulp.watch('./www/assets/sass/*.scss',['styles']);
+    gulp.watch('./www/assets/js/*.js',['compress-js']);
 });
